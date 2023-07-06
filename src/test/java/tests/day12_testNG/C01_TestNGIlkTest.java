69428a2791bf91435eb9ecb0bad200caded1336d
bulutluoz <abbulutluoz@gmail.com>
2023-07-06 11:55:21 +0200

    12.gun sonu

---
+++ src/test/java/tests/day12_testNG/C01_TestNGIlkTest.java
@@ -0,0 +1,25 @@
+package tests.day12_testNG;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C01_TestNGIlkTest {
+
+    @Test
+    public void test01(){
+        /*
+            POM'de driver olusturmak ve gerekli ayarlari yapmak icin
+            TestBase kullanmak yerine
+            Ayri bir Driver class'i icerisinde
+            bize driver döndürecek bir method olusturulmasi tercih edilmistir
+         */
+
+        Driver.getDriver().get("https://www.amazon.com");
+
+        ReusableMethods.bekle(5);
+
+        Driver.closeDriver();
+
+    }
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,27 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    public static WebDriver driver;
+
+    public static WebDriver getDriver(){
+
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        return driver;
+    }
+
+
+    public static void closeDriver(){
+        driver.close();
+    }
+}
