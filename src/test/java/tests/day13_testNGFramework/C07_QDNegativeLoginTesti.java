0dfe22d1959182ea40cd6b0eaa2bdf3cd84a2e11
bulutluoz <abbulutluoz@gmail.com>
2023-07-10 12:50:04 +0200

    13.gun sonu

---
+++ src/test/java/tests/day13_testNGFramework/C07_QDNegativeLoginTesti.java
@@ -0,0 +1,37 @@
+package tests.day13_testNGFramework;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QdPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C07_QDNegativeLoginTesti {
+
+    //1- https://www.qualitydemy.com/ anasayfasina gidin
+    //2- login linkine basin
+    //3- 3 farkli test method’u olusturun.
+    //	- gecerli username, gecersiz password
+    //	- gecersiz username, gecerli password
+    //	- gecersiz username, gecersiz password.
+    //4- Login butonuna basarak login olun
+    //5- Basarili olarak giris yapilamadigini test edin
+
+    @Test
+    public void gecersizPasswordTesti(){
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+
+        QdPage qdPage = new QdPage();
+
+        qdPage.ilkLoginLinki.click();
+
+        qdPage.emailKutusu.sendKeys(ConfigReader.getProperty("qdGecerliUsername"));
+        qdPage.passwordKutusu.sendKeys(ConfigReader.getProperty("qdGecersizPassword"));
+
+        qdPage.loginButonu.click();
+
+        Assert.assertTrue(qdPage.emailKutusu.isDisplayed());
+
+        Driver.closeDriver();
+    }
+}
