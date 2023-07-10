2f17618924c161d621ea2f86f247db984cb97f1f
bulutluoz <abbulutluoz@gmail.com>
2023-07-10 11:55:39 +0200

    13.gun sonu

---
+++ src/test/java/tests/day13_testNGFramework/C04_NutellaAramaTesti.java
@@ -0,0 +1,38 @@
+package tests.day13_testNGFramework;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C04_NutellaAramaTesti {
+
+    @Test
+    public void test01(){
+
+        // amazon anasayfaya gidin
+
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        // Nutella icin arama yapin
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys(ConfigReader.getProperty("amazonArananKelime")+ Keys.ENTER);
+
+        // sonuclarin Nutella icerdigini test edin
+        ReusableMethods.bekle(3);
+
+        String expectedIcerik = ConfigReader.getProperty("amazonArananKelime");
+        String actualSonucYazisi = amazonPage.sonucYaziElementi.getText();
+
+        System.out.println(ConfigReader.getProperty("amazonArananKelime"));
+        System.out.println(amazonPage.sonucYaziElementi.getText());
+        Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
+
+        // sayfayi kapatin
+
+        Driver.closeDriver();
+    }
+}
